9483e4fdd73464c2ea3ee645bb651ae244b7fcdd
Nitin Chaurasia <gs.nitin@gmail.com>
2020-01-18 22:36:25 -0500

    Circle CI test with H2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
---CREATE DATABASE 'restful_webservice' /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION=''N'' */;
+CREATE DATABASE 'restful_webservice' /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION=''N'' */;
 
 DROP TABLE IF EXISTS messages;
 create table messages
